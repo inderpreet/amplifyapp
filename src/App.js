6e56aced43e9f925a015884cbdf9ca2df9a31dca
Inderpreet Singh <er.inderpreet@gmail.com>
2021-06-14 15:49:44 -0400

    intermediate commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 function App() {
   return (
-    <div className="App container mt-5">
-      <h1 className="text-primary">
-        <i className="bi bi-diagram-2-fill"></i>
-        Test Application
-      </h1>
+    <div className="App image-frame container">
+      <div className="row top-row">
+        <div className="col top-row-left">col1</div>
+        <div className="col top-row-right">col1</div>
+      </div>
+      <div className="row row-two">Row2</div>
+
+      <div className="row row-bottom">Row3</div>
     </div>
   );
 }
