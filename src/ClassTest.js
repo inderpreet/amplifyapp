d7a4a3d767f02c952b5ac716a74ee3868044295a
Inderpreet Singh <Inderpreet.v1@gmail.com>
2021-07-27 08:29:06 -0400

    Add Scaffolding to square-badge in ClassTest.js
    Add row and column scaffolding to the square-badge responsive div for final layout.

--- src/ClassTest.js
+++ src/ClassTest.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import DataEntryBar from "./DataEntryBar";
 import Footer from "./Footer";
+import ImageMosiac from "./ImageMosiac";
 
 class ClassTest extends Component {
     constructor(props) {
@@ -29,6 +30,9 @@ class ClassTest extends Component {
     render() {
         return (
             <div className="container-fluid main-container-class">
+                <div>
+                    <ImageMosiac/>
+                </div>
 
                 <div className="row">
                     <nav className="navbar navbar-light bg-light">
@@ -47,16 +51,56 @@ class ClassTest extends Component {
 
                 {/*main content area*/}
                 <div className="row square-badge">
-                    Final Mosiac
-                    {this.state.images[0]}
-                    {this.state.images[1]}
-                    {this.state.texts[0]}
-                    {/*{this.state.bulletPoints[0]}*/}
-                    <ul>
-                        <li>Test List Item</li>
-                        {this.renderList(this.state.bulletPoints[0])}
-                    </ul>
+                    <div className="row">
+                        <div className="col">
+                            Left Column -1
+                        </div>
+                        <div className="col">
+                            RIght Column -1
+                        </div>
+                    </div>
+                    <div className="row" id="main-content-area">
+                        <div className="col" id="left-column-container">
+                            <div className="row" >
+                                <div className="col" id="small-image-1">
+                                    Small Image 1
+                                </div>
+                                <div className="col" id="small-image-2">
+                                    Small Image 2
+                                </div>
+                            </div>
+                            <div className="row" id="hero-image-container">
+                                Hero image
+                            </div>
+                        </div>
+                        <div className="col" id="right-column-container">
+                            <div className="row" id="bullet-list-container">
+                                <ul>
+                                    <li>One</li>
+                                    <li>Two</li>
+                                </ul>
+                            </div>
+                            <div className="row" id="square-image">
+                                Square Image
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        Bottom Bar
+                    </div>
+
+                    {/*Final Mosiac*/}
+                    {/*{this.state.images[0]}*/}
+                    {/*{this.state.images[1]}*/}
+                    {/*{this.state.texts[0]}*/}
+                    {/*/!*{this.state.bulletPoints[0]}*!/*/}
+                    {/*<ul>*/}
+                    {/*    <li>Test List Item</li>*/}
+                    {/*    {this.renderList(this.state.bulletPoints[0])}*/}
+                    {/*</ul>*/}
                 </div>
+
+
                 {/*Footer section*/}
                 <Footer/>
             </div>
